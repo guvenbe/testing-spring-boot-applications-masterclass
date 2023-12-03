cc66d5d7a227c47a48404a9b1659609ab3406ff4
Bora Guven <guvenbe@gmail.com>
2023-12-02 19:27:12 -0500

    mock

--- src/test/java/de/rieckpil/courses/book/management/BookSynchronizationListenerTest.java
+++ src/test/java/de/rieckpil/courses/book/management/BookSynchronizationListenerTest.java
@@ -12,7 +12,8 @@ import static org.mockito.Mockito.*;
 @ExtendWith(MockitoExtension.class)
 class BookSynchronizationListenerTest {
 
-  private final static String VALID_ISBN = "1234567891234";
+//  private final static String VALID_ISBN = BookSynchronizationListenerTest.VALID_ISBN;
+  public static final String VALID_ISBN = "1234567891234";
 
   @Mock
   private BookRepository bookRepository;
@@ -37,8 +38,8 @@ class BookSynchronizationListenerTest {
 
   @Test
   void shouldNotOverrideWhenBookAlreadyExists() {
-    BookSynchronization bookSynchronization = new BookSynchronization("1234567891234");
-    when(bookRepository.findByIsbn("1234567891234")).thenReturn(new Book());
+    BookSynchronization bookSynchronization = new BookSynchronization(BookSynchronizationListenerTest.VALID_ISBN);
+    when(bookRepository.findByIsbn(BookSynchronizationListenerTest.VALID_ISBN)).thenReturn(new Book());
     cut.consumeBookUpdates(bookSynchronization);
     verifyNoInteractions(openLibraryApiClient);
     verify(bookRepository, never()).save(any());
@@ -47,10 +48,31 @@ class BookSynchronizationListenerTest {
 
   @Test
   void shouldThrowExceptionWhenProcessingFails() {
+    BookSynchronization bookSynchronization = new BookSynchronization(BookSynchronizationListenerTest.VALID_ISBN);
+    when(bookRepository.findByIsbn(BookSynchronizationListenerTest.VALID_ISBN)).thenReturn(null);
+    when(openLibraryApiClient.fetchMetadataForBook(BookSynchronizationListenerTest.VALID_ISBN)).thenThrow(new RuntimeException("Something went wrong"));
+    assertThrows(RuntimeException.class, () -> cut.consumeBookUpdates(bookSynchronization));
+    verify(bookRepository, never()).save(any());
   }
 
   @Test
   void shouldStoreBookWhenNewAndCorrectIsbn() {
+    BookSynchronization bookSynchronization = new BookSynchronization(BookSynchronizationListenerTest.VALID_ISBN);
+    when(bookRepository.findByIsbn(BookSynchronizationListenerTest.VALID_ISBN)).thenReturn(null);
+    Book requestedBook = new Book();
+    requestedBook.setTitle("Java Book");
+    requestedBook.setIsbn(BookSynchronizationListenerTest.VALID_ISBN);
+    when(openLibraryApiClient.fetchMetadataForBook(BookSynchronizationListenerTest.VALID_ISBN)).thenReturn(requestedBook);
+    when(bookRepository.save(ArgumentMatchers.any())).then(invocation -> {
+      Book methodArgument = invocation.getArgument(0);
+      methodArgument.setId(1L);
+      return methodArgument;
+    });
+    cut.consumeBookUpdates(bookSynchronization);
+    verify(bookRepository).save(bookArgumentCaptor.capture());
+    assertEquals("Java Book", bookArgumentCaptor.getValue().getTitle());
+    assertEquals(1L, bookArgumentCaptor.getValue().getId());
+    assertEquals(BookSynchronizationListenerTest.VALID_ISBN, bookArgumentCaptor.getValue().getIsbn());
   }
 
 }
