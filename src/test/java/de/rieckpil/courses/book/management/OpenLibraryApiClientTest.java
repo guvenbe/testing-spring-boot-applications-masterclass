c104936eb02b2b63d772516526a7409f4debfe0a
Bora Guven <guvenbe@gmail.com>
2023-12-23 19:44:57 -0500

    json

--- src/test/java/de/rieckpil/courses/book/management/OpenLibraryApiClientTest.java
+++ src/test/java/de/rieckpil/courses/book/management/OpenLibraryApiClientTest.java
@@ -6,14 +6,18 @@ import io.netty.handler.timeout.WriteTimeoutHandler;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
 import okhttp3.mockwebserver.RecordedRequest;
+import org.junit.Before;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
+import reactor.netty.tcp.TcpClientConfig;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -24,15 +28,63 @@ class OpenLibraryApiClientTest {
   private OpenLibraryApiClient cut;
 
   private static final String ISBN = "9780596004651";
+  protected static String VALID_RESPONSE;
+  static {
+    try {
+      VALID_RESPONSE = new String(OpenLibraryApiClientTest.class
+        .getClassLoader()
+        .getResourceAsStream("stubs/openlibrary/success-" + ISBN + ".json")
+        .readAllBytes());
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 
-  private static String VALID_RESPONSE;
 
+
+  @BeforeEach
+  void setUp() throws IOException {
+    HttpClient httpClient = HttpClient.create()
+      .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 2_000)
+      .doOnConnected(connection ->
+        connection.addHandlerLast(new ReadTimeoutHandler(2))
+          .addHandlerLast(new WriteTimeoutHandler(2)));
+
+    this.mockWebServer = new MockWebServer();
+    this.mockWebServer.start();
+
+   this.cut = new OpenLibraryApiClient(
+     WebClient.builder()
+      .clientConnector(new ReactorClientHttpConnector(httpClient))
+      .baseUrl(this.mockWebServer.url("/").toString())
+      .build());
+
+
+  }
   @Test
   void notNull() {
+    assertNotNull(cut);
+    assertNotNull(mockWebServer);
   }
 
   @Test
   void shouldReturnBookWhenResultIsSuccess() throws InterruptedException {
+    MockResponse mockResponse = new MockResponse()
+      .addHeader("Content-Type", "application/json charset=utf-8")
+      .setBody(VALID_RESPONSE);
+
+    this.mockWebServer.enqueue(mockResponse);
+
+    Book result = cut.fetchMetadataForBook(ISBN);
+    assertEquals(ISBN, result.getIsbn());
+    assertEquals("Head second Java", result.getTitle());
+    assertEquals("https://covers.openlibrary.org/b/id/388761-S.jpg", result.getThumbnailUrl());
+    assertEquals("Kathy Sierra", result.getAuthor());
+    assertEquals("O'Reilly", result.getPublisher());
+    assertEquals(42, result.getPages());
+    assertEquals("n.A", result.getDescription());
+    assertEquals("n.A", result.getGenre());
+    assertNull(result.getId());
   }
 
   @Test
--- src/test/java/de/rieckpil/courses/book/management/OpenLibraryRestTemplateApiClientTest.java
+++ src/test/java/de/rieckpil/courses/book/management/OpenLibraryRestTemplateApiClientTest.java
@@ -6,10 +6,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.test.web.client.match.MockRestRequestMatchers;
 import org.springframework.test.web.client.response.MockRestResponseCreators;
+import org.springframework.web.client.HttpServerErrorException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -57,13 +59,83 @@ class OpenLibraryRestTemplateApiClientTest {
 
   @Test
   void shouldReturnBookWhenResultIsSuccessButLackingAllInformation() {
+    String response =
+      """
+     {
+      "9780596004651": {
+        "publishers": [
+          {
+            "name": "O'Reilly"
+          }
+        ],
+        "title": "Head second Java",
+        "authors": [
+          {
+            "url": "https://openlibrary.org/authors/OL1400543A/Kathy_Sierra",
+            "name": "Kathy Sierra"
+          }
+        ],
+        "number_of_pages": 42,
+        "cover": {
+          "small": "https://covers.openlibrary.org/b/id/388761-S.jpg",
+          "large": "https://covers.openlibrary.org/b/id/388761-L.jpg",
+          "medium": "https://covers.openlibrary.org/b/id/388761-M.jpg"
+        }
+       }
+     }
+    """;
+    this.mockRestServiceServer
+      .expect(MockRestRequestMatchers.requestTo(Matchers.containsString("/api/books")))
+      .andRespond(
+        MockRestResponseCreators.withSuccess(response, MediaType.APPLICATION_JSON));
+
+//    this.mockRestServiceServer
+//      .expect(MockRestRequestMatchers.requestTo(Matchers.containsString("/duke/42")))
+//      .andRespond(
+//        MockRestResponseCreators.withSuccess(response, MediaType.APPLICATION_JSON));
+
+    Book result = cut.fetchMetadataForBook(ISBN);
+
+    assertEquals(ISBN, result.getIsbn());
+    assertEquals("Head second Java", result.getTitle());
+    assertEquals("https://covers.openlibrary.org/b/id/388761-S.jpg", result.getThumbnailUrl());
+    assertEquals("Kathy Sierra", result.getAuthor());
+    assertEquals("O'Reilly", result.getPublisher());
+    assertEquals(42, result.getPages());
+    assertEquals("n.A", result.getDescription());
+    assertEquals("n.A", result.getGenre());
+    assertNull(result.getId());
+
+
+
+//    this.mockRestServiceServer.verify();
+
   }
 
   @Test
   void shouldPropagateExceptionWhenRemoteSystemIsDown() {
+    assertThrows(HttpServerErrorException.class, () -> {
+      this.mockRestServiceServer
+        .expect(MockRestRequestMatchers.requestTo("/api/books?jscmd=data&format=json&bibkeys=" + ISBN))
+        .andRespond(
+//          MockRestResponseCreators.withServerError());
+          MockRestResponseCreators.withStatus(HttpStatus.BAD_GATEWAY));
+      cut.fetchMetadataForBook(ISBN);
+    });
   }
 
   @Test
   void shouldContainCorrectHeadersWhenRemoteSystemIsInvoked() {
-  }
+      this.mockRestServiceServer
+//      .expect(MockRestRequestMatchers.requestTo("/api/books?jscmd=data&format=json&bibkeys=" + ISBN))
+        .expect(MockRestRequestMatchers.requestTo(Matchers.containsString(ISBN)))
+        .andExpect(MockRestRequestMatchers.header("X-Custom-Auth", "Duke42"))
+        .andExpect(MockRestRequestMatchers.header("X-Customer-Id", "42"))
+        .andRespond(
+          MockRestResponseCreators.withSuccess(new ClassPathResource("/stubs/openlibrary/success-" + ISBN + ".json")
+            , MediaType.APPLICATION_JSON));
+      Book result = cut.fetchMetadataForBook(ISBN);
+      assertNotNull(result);
+    }
+
 }
